ae13f2356b4ac4135d458b0a7fad88c5ec120f5e
P3401971 <P3401971@L34001100628.ness.com>
2016-09-21 14:39:16 +0200

    helou

--- Algoritmynet/src/algoritmynet/Algoritmynet.java
+++ Algoritmynet/src/algoritmynet/Algoritmynet.java
@@ -15,7 +15,7 @@ public class Algoritmynet {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        helou
     }
     
 }
