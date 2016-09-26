98b6825b89215b06a1893bb904c0e6d7cde3f4d2
P3401971 <P3401971@L34001100628.ness.com>
2016-09-26 15:49:02 +0200

    26.9.2016 15:48hod v praci

---
+++ Algoritmynet/src/Metody/PredavaniParametru.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Metody;
+
+/**
+ * @author P3401971
+ */
+public class PredavaniParametru {
+    public static void main(String[] args) {
+        //tady bude moje instance
+    }
+    private String kind;
+    private String sound;
+    
+    public void coJsem() {
+        System.out.println("Tohle je test typu: " + getKind() + "\na typu 2: " + getSound());
+        System.out.println("Funguje?");
+    }    
+    public String getKind(){
+        return kind;
+    }   
+    
+    public String getSound(){
+        return sound;
+    }
+    
+    public void setKind(String kind){
+        this.kind = kind;
+    }
+    
+    public void setSound(String sound){
+        this.sound = sound;
+    }
+
+}
