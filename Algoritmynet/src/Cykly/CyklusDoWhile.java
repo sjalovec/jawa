fc7c742eee967f72e50dc2ac713cf9068627d771
P3401971 <P3401971@L34001100628.ness.com>
2016-09-30 10:14:27 +0200

    30.9.2016 10:14hod v praci (chodov)

--- Algoritmynet/src/Cykly/CyklusDoWhile.java
+++ Algoritmynet/src/Cykly/CyklusDoWhile.java
@@ -2,14 +2,20 @@ package Cykly;
 
 public class CyklusDoWhile {
     public static void main(String[] args) {
-        public static int euklid (int a, int b){
+    //euklid(133,15); //moje volání, nneí potřeba pro sout
+        System.out.println("výsledek je: "+euklid(133,15)); //vypise na obrazovku výsledek pocítaní, podle vložených promenných
+    }
+    public static int euklid (int a, int b){
             if (a < 1 || b < 1) return -1;
             
             int spolecnyDelitel = 0;
             int remajdr = 0;
             
-            
+            do{
+                remajdr = a % b; //v tento okamžik v poslední iteraci platí ona podmínka, ze zbytku 0
+                a = b; //pro jistotu posuneme dal
+                b = remajdr; //v b je proto 0, v a je euklid
+            }while (b != 0);
+            return a;
         }
-    }
-
 }
---
+++ Algoritmynet/src/Cykly/CyklusFor1.java
@@ -0,0 +1,5 @@
+package Cykly;
+
+public class CyklusFor1 {
+
+}
