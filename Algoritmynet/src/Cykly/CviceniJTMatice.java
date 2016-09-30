51459b4ac07e65dd29cce2d63fea0cbdf9e0bad7
P3401971 <P3401971@L34001100628.ness.com>
2016-09-30 16:21:19 +0200

    30.9.2016 16:21hod v praci (Chodov)

--- Algoritmynet/src/Cykly/CviceniJTMatice.java
+++ Algoritmynet/src/Cykly/CviceniJTMatice.java
@@ -5,17 +5,57 @@ import java.util.Random;
 public class CviceniJTMatice {
     public static void main(String[] args) {
         
-        int n = 5;
-        int m = 6;
-        for (int i = 0; i< n;i++){
-            for (int j = 0; j < m; j++){
-                int[][] maticeAPole = new int[n][m];
-                Random nahCislo = new Random();
-                System.out.print(maticeAPole[i][j] = nahCislo.nextInt(5));
+        Random velikostMaticeX = new Random();
+        Random velikostMaticeY = new Random();
+        Random velikostMaticeZ = new Random();
+        
+        int x = velikostMaticeX.nextInt(4)+2; // zadání <2;5>
+        int y = velikostMaticeY.nextInt(4)+2; // zadání <2;5>
+        int z = velikostMaticeZ.nextInt(4)+2; // zadání <2;5>
+        
+        int[][] maticeAPole = new int[x][y]; //vytvoření pole pro matici A
+        int[][] maticeBPole = new int[y][z]; //vytvoření pole pro matici B
+        int[][] maticeCPole = new int[x][z]; //vytvoření pole pro matici C
+        
+        int i, j, k, l, m, n;
+        
+        //první matice
+        System.out.println("Matice A o velikosti: "+x+"x"+y);
+        for (i = 0; i< x;i++){
+            for (j = 0; j < y; j++){   
+                Random nahCisloA = new Random(); //vygenerování náhodného čísla pro matici
+                System.out.print(maticeAPole[i][j] = nahCisloA.nextInt(5)); //přiřazení náh. čísla do pole a print na obraz
                 System.out.print(" ");
             }
             System.out.println("");
         }
+        
+        //druhá matice
+        System.out.println("");
+        System.out.println("Matice B o velikosti: "+y+"x"+z);
+        for (k = 0; k < y; k++){
+            for (l = 0; l < z; l++){
+
+                //Random nahCisloB = new Random();  //tahle matice není náhodná 
+                System.out.print(maticeBPole[k][l] = ((2*k)+l));
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
+        //násobíme matice
+        System.out.println("");
+        System.out.println("Výsledná matice násobení: "+x+"x"+z);
+        {
+            for (m = 0; m<z;m++){
+                for (n = 0; n<x;n++){
+                    System.out.print(maticeCPole[m][n] = maticeCPole[m][n]+(maticeAPole[i][j] * maticeBPole[k][l]));
+                    System.out.print(" ");
+                }
+            System.out.println("");
+            }
+        }
+        
+        
     }
 
 }
