a8a9dacf8f1536c48a1819c29ca6e3b2947a4397
sjalo <sjalo@chuppa>
2016-10-05 22:55:21 +0200

    5.10.2016 - Doma

--- Algoritmynet/src/Vyjimky/ukolOdTJPoleHledani.java
+++ Algoritmynet/src/Vyjimky/ukolOdTJPoleHledani.java
@@ -1,8 +1,32 @@
 package Vyjimky;
 //ma najit promenou v poli pomocí rekurze
+
 public class ukolOdTJPoleHledani {
+
     public static void main(String[] args) {
-        
+        //volání metody
+        rekurzeTestCyklus(5);
+
     }
- int[] mojePole = {1,3,5,7,9,11,13,15,17,19};
+
+    public static int rekurzePole(int vstupniHodnota) {
+        int[] poleHodnotRek = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
+    }
+
+    public static int rekurzeTestCyklus(int cisloCoHledam) {
+        int[] poleHodnot = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
+        int pomProm = 0;
+        for (int i = 0; i < poleHodnot.length; i++) {
+            if (poleHodnot[i] == cisloCoHledam) {
+                //System.out.println("Tvoje cislo je na pozici: "+i);
+                pomProm = i;
+                break;
+            } else //System.out.println("Tvoje cislo neni v seznamu");
+            {
+                continue;
+            }
+        }
+        System.out.println("Tvoje cislo je: " + cisloCoHledam + " a je na pozici: " + pomProm);
+        return 0;
+    }
 }
