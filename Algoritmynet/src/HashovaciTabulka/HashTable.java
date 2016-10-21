2cb314cb126da3aff37eb811546f4829ae88e84f
P3401971 <P3401971@L34001100628.ness.com>
2016-10-21 15:08:47 +0200

    21.10.2016 15:08hod v praci (Chodov) kapitola 20 začátek

---
+++ Algoritmynet/src/HashovaciTabulka/HashTable.java
@@ -0,0 +1,31 @@
+package HashovaciTabulka;
+
+public class HashTable<KEY, VALUE> implements TableIface<KEY, VALUE>{
+    
+    //Pomer zaplneni pri kterem dojde k vytvoreni nove (vetsi) tabulky
+    private final float LOAD_FACTOR = 0.75f;
+    
+    //Pomer zaplneni pri kterem dojde k vytvoreni nove (mensi tabulky)
+    private final float COLAPSE_RATIO = 0.1f;
+    
+    //Hodnota, pod kterou nikdy neklesne velikost tabulky
+    private final int INITIAL_CAPACITY;
+    
+    //počet uložených prvků
+    private int size = 0;
+    private Entry<KEY, VALUE>[] table;
+    
+    //Zkonstruuje hashovaci tabulku s vychozi kapacitou 10
+    public HashTable() {
+        this(10); //výchozí kapacita
+    }
+    
+    //zkonstruuje hashovaci tabulku
+    public HashTable (int initialCapacity) {
+        if (initialCapacity <= 0) {
+            thrown new IllegalArgumentException ("kapacita nesmi byt zaporna");
+        }
+    }
+    
+
+}
