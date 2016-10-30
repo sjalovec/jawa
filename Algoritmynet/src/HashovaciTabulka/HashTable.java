8fae83e609d8e39f9f42e61bf522a63db8fa2451
sjalo <sjalo@chuppa>
2016-10-30 19:15:38 +0100

    30.10.2016 - Doma

--- Algoritmynet/src/HashovaciTabulka/HashTable.java
+++ Algoritmynet/src/HashovaciTabulka/HashTable.java
@@ -1,31 +1,38 @@
 package HashovaciTabulka;
 
-public class HashTable<KEY, VALUE> implements TableIface<KEY, VALUE>{
-    
+public class HashTable<KEY, VALUE> implements TableIface<KEY, VALUE> {
+
     //Pomer zaplneni pri kterem dojde k vytvoreni nove (vetsi) tabulky
     private final float LOAD_FACTOR = 0.75f;
-    
+
     //Pomer zaplneni pri kterem dojde k vytvoreni nove (mensi tabulky)
     private final float COLAPSE_RATIO = 0.1f;
-    
+
     //Hodnota, pod kterou nikdy neklesne velikost tabulky
     private final int INITIAL_CAPACITY;
-    
+
     //počet uložených prvků
     private int size = 0;
     private Entry<KEY, VALUE>[] table;
-    
+
     //Zkonstruuje hashovaci tabulku s vychozi kapacitou 10
     public HashTable() {
         this(10); //výchozí kapacita
     }
-    
+
     //zkonstruuje hashovaci tabulku
-    public HashTable (int initialCapacity) {
+    public HashTable(int initialCapacity) {
         if (initialCapacity <= 0) {
-            thrown new IllegalArgumentException ("kapacita nesmi byt zaporna");
+            thrown new IllegalArgumentException("Kapacita nesmi byt mensi nez nula.");
         }
+        this.INITIAL_CAPACITY = initialCapacity;
+        this.table = new Entry[initialCapacity];
     }
-    
+
+    private class Entry<KEY, VALUE> {
+
+        private KEY key;
+        private VALUE value;
+    }
 
 }
--- Algoritmynet/src/HashovaciTabulka/TableIface.java
+++ Algoritmynet/src/HashovaciTabulka/TableIface.java
@@ -1,29 +1,28 @@
 package HashovaciTabulka;
 
 //rozhraní tabulky
-
 import java.util.Collection;
 
 public interface TableIface<KEY, VALUE> {
-    
+
     //Vlozi prvek do tabulky, pokud jiz prvek s danym klicem existuje, tak bude nahrazen
     public VALUE put(KEY key, VALUE Value);
-    
+
     //Odstrani prvek odpovidajici danemu klici
     public VALUE remove(KEY key);
-    
+
     //vrátí hodnotu prvku podle klíce
     public VALUE get(KEY key);
-    
+
     //dotaz na přítomnost
     public boolean contains(KEY key);
-    
+
     //dotaz na pocet ulozených prvku
     public int size();
-    
+
     //kolekce všech uložených hodnot
     public Collection<VALUE> values();
-    
+
     //kolekce vsech klicu
     public Collection<KEY> keys();
 }
