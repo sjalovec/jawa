e9f2c0224e06a5c71f1c6b128bac9c24d9a3b2d1
P3401971 <P3401971@L34001100628.ness.com>
2016-11-11 15:59:10 +0100

    Kalkulačka

---
+++ Calculator/src/action/impl/PlusAction.java
@@ -0,0 +1,18 @@
+package action.impl;
+//Operace scitani
+
+import action.AbstractOperatorAction;
+import javax.swing.JTextField;
+import model.CalculatorModel;
+
+public class PlusAction extends AbstractOperatorAction {
+
+    public PlusAction(JTextField resultField, CalculatorModel model) {
+        super(resultField, model);
+    }
+
+    @Override
+    public void performActualOperatorAction() {
+        getModel().setStoredResult(getModel().getStoredResult() + Double.valueOf(getModel().getCurrentInput()));
+    }
+}
