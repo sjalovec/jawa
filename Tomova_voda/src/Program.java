d4c61193704dfcf2f8baff863e65f37546939aae
P3401971 <P3401971@L34001100628.ness.com>
2016-11-23 16:07:30 +0100

    Konstruktory zboží hotovo. Sklad metody hotovo. Vyroba hotovo. Volání výroby z main. Úprava linky na stav vyrobku.

--- Tomova_voda/src/Program.java
+++ Tomova_voda/src/Program.java
@@ -3,6 +3,7 @@
 //public static Kolo dement; -TGIF 2016
 
 import storage.Sklad;
+import vyroba.Linka;
 import vyrobky.Dvoukolo;
 import vyrobky.Liberta;
 import vyrobky.Trikolka;
@@ -14,26 +15,11 @@ public class Program {
 
     public static void main(String[] args) {
         vyroba.Linka.vyrobVyrobek("Trikolka");
-        /*
-Dvoukolo dk = new Dvoukolo(new Kolo(), new Kolo(), new Ram(), new Sedlo(), new Sedlo());
-        dk.vypisTypVyrobku();
-        dk.vypisText();
-
-        Liberta l = new Liberta(new Kolo(), new Kolo(), new Ram(), new Sedlo());
-        l.vypisTypVyrobku();
-        l.vypisText();
-        
-        Trikolka tk = new Trikolka(new Kolo(), new Kolo(), new Kolo(), new Ram(), new Sedlo());
-        tk.vypisTypVyrobku();
-        tk.vypisText();
-
-        Sklad.stavZbozi("kolo");
-        
-        Sklad.objednejZbozi("kolo", 5);
-*/
         Sklad.stavZbozi("kolo");
         Sklad.stavZbozi("ram");
         Sklad.stavZbozi("sedlo");
+        
+        System.out.println("Je vyrobeno: "+Linka.pocetVyrobku()+" ks výrobků.");
 
     }
 
--- Tomova_voda/src/vyroba/Linka.java
+++ Tomova_voda/src/vyroba/Linka.java
@@ -8,27 +8,34 @@ import zbozicko.Ram;
 import zbozicko.Sedlo;
 
 public class Linka {
-    //public void vyrobLiberta<ENTITY> (vyrobek) {}  
+    public static int pocetVyrobku = 0;
+    
     public static void vyrobVyrobek(String vyrobek) {
         switch (vyrobek) {
             case "Liberta":
                 Liberta l = new Liberta(new Kolo(), new Kolo(), new Ram(), new Sedlo());
                 l.vypisTypVyrobku();
                 l.vypisText();
+                pocetVyrobku++;
                 break;
             case "Dvoukolo":
                 Dvoukolo dk = new Dvoukolo(new Kolo(), new Kolo(), new Ram(), new Sedlo(), new Sedlo());
                 dk.vypisTypVyrobku();
                 dk.vypisText();
+                pocetVyrobku++;
                 break;
             case "Trikolka":
                 Trikolka tk = new Trikolka(new Kolo(), new Kolo(), new Kolo(), new Ram(), new Sedlo());
                 tk.vypisTypVyrobku();
                 tk.vypisText();
+                pocetVyrobku++;
                 break;
             default:
                 System.out.println("Nebyl zadán správný výrobek.");
                 break;
         }
+    }
+    public static int pocetVyrobku() {
+        return pocetVyrobku;
     }
 }
