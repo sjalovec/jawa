c901066420e9ccc43a1d94666ee83a4154dbe5a6
P3401971 <P3401971@L34001100628.ness.com>
2016-12-05 17:31:52 +0100

    Novy projekt od TGIF.
    v2:
    Odstranění nepotřebných tříd a IF.
    Vytvoření fungující main methody.

--- SpojSeznamTGIF/src/program/Program.java
+++ SpojSeznamTGIF/src/program/Program.java
@@ -1,30 +1,42 @@
 package program;
 
-import code.MujLinkedList;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        
-        //FileReader fr = new FileReader("d:/soubor.txt");
-        //System.out.println(fr);
-        BufferedReader in = new BufferedReader(new FileReader("d:/soubor.txt"));
-        List<String> mujLinkList = new ArrayList<String>();
-        mujLinkList = in.readLine();
-        //String line = in.readLine();
-        System.out.println(mujLinkList);
-        
-        
-
+        System.out.println("Vyber, kterou chceš variantu:");
+        System.out.println("1. Array list");
+        System.out.println("2. Linked list");
+        System.out.println("3. Ukončit");
+        Scanner sc = new Scanner(System.in);
+        int vyber = sc.nextInt();
+        switch(vyber) {
+            case(1):
+                System.out.println("Vybrals 1");
+                BufferedReader in = new BufferedReader(new FileReader("d:/soubor.txt"));
+                List<String> mujArrayList = new ArrayList<String>();
+                mujArrayList.add(in.readLine());
+                System.out.println(mujArrayList);
+                break;
+            case(2):
+                System.out.println("Vybrals 2");
+                BufferedReader in2 = new BufferedReader(new FileReader("d:/soubor.txt"));
+                List<String> mujArrayList2 = new ArrayList<String>();
+                mujArrayList2.add(in2.readLine());
+                System.out.println(mujArrayList2);
+                break;
+            default:
+                System.out.println("Vybrals na kokot.");
+                break;
+        };
     }
-
 }
 
 /*ja chci po tobe abys znovu naprogramoval spojovej seznam, bez divani se kamkoliv!!! a taky zaroven dynamicky alokovany pole a abych pak mohl spustit program nejak takto:
