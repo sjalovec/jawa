a64bba35f25369ce459eed273fc517c405ad331c
P3401971 <P3401971@L34001100628.ness.com>
2016-12-05 17:07:09 +0100

    Novy projekt od TGIF

--- SpojSeznamTGIF/src/code/SouborReady.java
+++ SpojSeznamTGIF/src/code/SouborReady.java
@@ -3,6 +3,6 @@ package code;
 import java.io.FileReader;
 
 public class SouborReady{
-    FileReader fr = new FileReader("soubor.txt");
+    
     
 }
--- SpojSeznamTGIF/src/program/Program.java
+++ SpojSeznamTGIF/src/program/Program.java
@@ -1,11 +1,26 @@
 package program;
 
 import code.MujLinkedList;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Program {
 
-    public static void main(String[] args) {
-        MujLinkedList mll = new MujLinkedList();
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        
+        //FileReader fr = new FileReader("d:/soubor.txt");
+        //System.out.println(fr);
+        BufferedReader in = new BufferedReader(new FileReader("d:/soubor.txt"));
+        List<String> mujLinkList = new ArrayList<String>();
+        mujLinkList = in.readLine();
+        //String line = in.readLine();
+        System.out.println(mujLinkList);
+        
         
 
     }
