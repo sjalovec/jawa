7451bb32b3ff8d0905b87219a87256ea15cd9d0d
P3401971 <P3401971@L34001100628.ness.com>
2019-04-09 14:59:35 +0200

    Algoritmy.net podruhé

---
+++ AlgoritmyNet2019/src/PrvniProgram/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PrvniProgram;
+
+/**
+ *
+ * @author P3401971
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Ahoj svete");
+    }
+    
+}
