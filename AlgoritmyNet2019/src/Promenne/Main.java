c63cf2c04f9eb9c2760107221b5a6b0056dd3f96
P3401971 <P3401971@L34001100628.ness.com>
2019-04-10 17:06:37 +0200

    Algoritmy.net podruhé

---
+++ AlgoritmyNet2019/src/Promenne/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Promenne;
+
+/**
+ *
+ * @author P3401971
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    
+    int i1;
+    i1 = 1500;
+    }
+}
