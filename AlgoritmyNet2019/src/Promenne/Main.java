2572a1ac46711876c9d5debad03b1f0a5815f780
P3401971 <P3401971@L34001100628.ness.com>
2019-04-23 11:02:40 +0200

    Algoritmy.net podruhé 23.4.2019

--- AlgoritmyNet2019/src/Promenne/Main.java
+++ AlgoritmyNet2019/src/Promenne/Main.java
@@ -15,5 +15,19 @@ public class Main {
     
     int i1;
     i1 = 1500;
+    
+    double d1 = 100.5;
+    
+    float f1 = 1.5f;
+    
+    char ch1 = 'a';
+    
+    boolean b1 = true;
+    
+        System.out.println(i1);
+        System.out.println(d1);
+        System.out.println(f1);
+        System.out.println(ch1);
+        System.out.println(b1);
     }
 }
